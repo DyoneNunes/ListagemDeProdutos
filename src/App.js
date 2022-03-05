8d3708794d7c77e7c7d82d321d06309611b62687
DyoneNunes <43630321+DyoneNunes@users.noreply.github.com>
2022-03-05 17:43:34 -0300

    primeira Branch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Table from './Table/Table';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Container">
+        <Table/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
